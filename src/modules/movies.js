e8f157bae9ed552493889e64bff0042257a0ef45
Betsegaw Sebsibe <betsesebe@gmail.com>
2022-08-30 11:05:54 +0300

    Add getLiked and addLiked functions

--- src/modules/movies.js
+++ src/modules/movies.js
@@ -1,7 +1,7 @@
 class Movies {
-  // constructor() {
-  //   this.movies = [];
-  // }
+  constructor() {
+    this.likes = [];
+  }
 
   getMovie = async () => {
     const data = await fetch('https://api.tvmaze.com/shows');
@@ -13,13 +13,21 @@ class Movies {
     }
   }
 
+  getLikes = async () => {
+    const likedList = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/g47Ybpe3Iv9MLdD87d0m/likes').then((response) => response.json());
+    this.likes = likedList;
+  }
+
   displayMovies = async () => {
+    await this.getLikes();
     const div = document.querySelector('.grid-container');
     const response = await this.getMovie();
     for (let movies = 1; movies <= 20; movies += 1) {
       const card = document.createElement('div');
       card.classList.add('card');
       const movie = response[movies];
+      const index = this.likes.findIndex((like) => like.item_id === movie.id);
+      const msgLikes = index >= 0 ? this.likes[index].likes : 0;
       card.id = `${movie.id}`;
       card.innerHTML += `
       <div class="container">
@@ -30,11 +38,35 @@ class Movies {
       </div>
       <div class="title-container">
         <h4><b>${movie.name}</b></h4> 
-        <span> <i class="fa fa-heart" data-id=${movie.id}></i> 0Like(s)</span>
+        <div class="like-icon"> <i class="fa fa-heart" data-pos=${movie.id}></i> <span id="movie-id"> ${msgLikes} </span> Like(s)</div>
       </div>
       `;
       div.append(card);
     }
+    const likeButtons = document.querySelectorAll('.fa-heart');
+    likeButtons.forEach((btn) => {
+      btn.addEventListener('click', () => {
+        const movieId = parseInt(btn.getAttribute('data-pos'), 10);
+        this.addLike(movieId, btn);
+        btn.disabled = true;
+        btn.style.color = 'red';
+      }, { once: true });
+    });
+  }
+
+  addLike = async (itemId, likeButton) => {
+    await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/g47Ybpe3Iv9MLdD87d0m/likes', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ item_id: itemId }),
+    }).then((response) => response.text(response)).then((json) => json);
+    await this.getLikes();
+    // const span = document.getElementById('movie-id');
+    const index = this.likes.findIndex((like) => like.item_id === itemId);
+    const msgLikes = index >= 0 ? this.likes[index].likes : 0;
+    likeButton.nextElementSibling.innerHTML = msgLikes;
   }
 }
 
