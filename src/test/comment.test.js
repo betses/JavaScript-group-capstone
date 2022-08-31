f4621b68596003af9c862e932ecac46721827034
Hamid Ali <hammali99@gmail.com>
2022-08-31 22:39:18 +0500

    added unit test for comment count

---
+++ src/test/comment.test.js
@@ -0,0 +1,28 @@
+import Movies from '../modules/movies.js';
+
+global.fetch = jest.fn(() => Promise.resolve({
+  json: () => Promise.resolve(
+    [
+        {
+            "username": "hamid",
+            "creation_date": "2022-08-31",
+            "comment": "sas"
+        },
+        {
+            "creation_date": "2022-08-31",
+            "username": "ddd",
+            "comment": "ddd"
+        }
+    ],
+  ),
+}));
+
+describe('comment counter tests using Jest', () => {
+  const movies = new Movies();
+
+  test('test Comment Count ', async () => {
+    const response = await movies.getComment(4);
+
+    expect(response.length).toBe(2);
+  });
+});
